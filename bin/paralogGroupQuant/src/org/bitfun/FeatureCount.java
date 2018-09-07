4f9799915e569bed5ed94de14aebb663d6485f9f
vitor <vitorcoelho>
2018-09-07 18:17:15 -0300

    Update FeatureCount-class

--- bin/paralogGroupQuant/src/org/bitfun/FeatureCount.java
+++ bin/paralogGroupQuant/src/org/bitfun/FeatureCount.java
@@ -2,65 +2,57 @@ package org.bitfun;
 
 import java.util.List;
 
-public class FeatureCount {
+class FeatureCount {
     private String groupId;
     private List<String> geneList;
     private float[] counts;
 
-    public FeatureCount(String groupId) {
-        this.groupId = groupId;
-    }
-
-    public List<String> getGeneList() {
-        return geneList;
-    }
-
-    public void setGeneList(List<String> geneList) {
+    void setGeneList(List<String> geneList) {
         this.geneList = geneList;
     }
 
-    public float[] getCounts() {
-        return counts;
-    }
-
-    public void setCounts(float[] counts) {
+    void setCounts(float[] counts) {
         this.counts = counts;
     }
 
-    public void updateCounts(int index, float value){
+    void updateCounts(int index, float value){
 
         this.counts[index] += value;
 
     }
 
-    public void updateGeneList(String gene){
+    void updateGeneList(String gene){
         this.geneList.add(gene);
     }
 
-    public String getGeneListToString(){
-        String output = "";
+    String getGeneListToString(){
+        StringBuilder output = new StringBuilder();
         if (geneList.size() ==1 )
             return geneList.get(0);
         for (String s: geneList){
-            if(output.equals("")){
-                output = s;
+            if(output.toString().equals("")){
+                output = new StringBuilder(s);
             }else{
-                output = output + "|" + s;
+                output.append("|").append(s);
             }
         }
-        return output;
+        return output.toString();
     }
 
-    public String getCountListToString(){
-        String output = "";
+    String getCountListToString(){
+        StringBuilder output = new StringBuilder();
         if(counts.length == 1)
             return String.valueOf(counts[0]);
         for(float f : counts){
-            if(output.equals(""))
-                output = String.valueOf(f);
+            if(output.toString().equals(""))
+                output = new StringBuilder(String.valueOf(f));
             else
-                output = output + "\t" + String.valueOf(f);
+                output.append("\t").append(String.valueOf(f));
         }
-        return output;
+        return output.toString();
+    }
+
+    void setGroupId(String groupId) {
+        this.groupId = groupId;
     }
 }
